65a7f40bcc8424415a8c842e465e9748b86612cc
TatianaNaberezhnykh <t.naberezhnykh@gmail.com>
2024-10-12 20:16:57 +0300

    Samokat testing

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://5e05ce8f-b170-471b-801d-24c4056ac9f0.serverhub.praktikum-services.ru"
+
+# CREATE_ORDER хранит путь к API-методу для создания нового заказа.
+CREATE_ORDER = "/api/v1/orders"
+
+# PUT_ORDER хранит путь к API-методу для создания нового набора.
+PUT_ORDER = "/api/v1/orders/track"
